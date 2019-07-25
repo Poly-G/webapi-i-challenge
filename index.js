5e0a1abb6af2eebdd46f54b4a0f2eb8e1abf2395
Poly Goveia <poly.goveia@gmail.com>
2019-07-24 18:55:26 -0700

    install npm and import express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(4000, () => {
+  console.log("server listening on port 4000");
+});
