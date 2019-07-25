d578ca4f7257b9e09df5fdac3d0b6476cecc7cf1
Poly Goveia <poly.goveia@gmail.com>
2019-07-24 19:00:39 -0700

    add get request

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/db", (req, res) => {
+  db.find()
+    .then(db => {
+      res.status(200).json(db);
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, err });
+    });
+});
+
 server.listen(4000, () => {
   console.log("server listening on port 4000");
 });
