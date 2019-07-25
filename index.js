204367e108cc13bfd9d542f85e6a1c047e950c1e
Poly Goveia <poly.goveia@gmail.com>
2019-07-24 20:25:54 -0700

    fix delete and meet mvp

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ server.get("/api/users/:id", (req, res) => {
 server.post("/api/users", (req, res) => {
   const { name, bio } = req.body;
 
+  // if there is no name or bio respond with 400 + message && cancel the request
   if (!name || !bio) {
     res
       .status(400)
@@ -62,22 +63,20 @@ server.post("/api/users", (req, res) => {
       res.status(201).json({ name, bio });
     })
     .catch(() => {
-      res
-        .status(500)
-        .json({
-          error: "There was an error while saving the user to the database"
-        });
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      });
       return;
     });
 });
 
 server.delete("/api/users/:id", (req, res) => {
-  const { id } = request.params;
+  const { id } = req.params;
 
   db.remove(id)
     .then(deleted => {
       if (deleted) {
-        res.status(204).end();
+        res.status(204).json(deleted);
       } else {
         res.status(404).json({ success: false, message: "Not deleted" });
       }
